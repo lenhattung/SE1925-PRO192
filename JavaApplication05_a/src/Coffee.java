c049599d4c311d078c39e902b6a321e9c5b5ab46
Tung Le <lenhattung@gmail.com>
2025-05-26 13:21:49 +0700

    ud
    ud

---
+++ JavaApplication05_a/src/Coffee.java
@@ -0,0 +1,22 @@
+
+public class Coffee {
+
+    // propeties
+    private String name;
+    private double weight;
+    private double price;
+
+    // methods
+    // constructor
+    public Coffee() {
+        this.name = "";
+        this.weight = 0;
+        this.price = 0;
+    }
+
+    public Coffee(String n, double w, double p) {
+        this.name = n;
+        this.weight = w;
+        this.price = p;
+    }
+}
---
+++ JavaApplication05_a/src/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tungi
+ */
+public class Main {
+    public static void main(String[] args) {
+        Coffee cf1 = new Coffee("Trung Nguyen", 0.5, 150000);
+        
+        String s1 = new String("Welcome TT Phap - Emanuel Macron");
+    }
+}
